ebb0a4148452a116dca9ecb577335dc41a19dd07
fm-cduffy <cduffy@flexmanage.com>
2017-04-05 13:55:06 -0700

    adding scripts

---
+++ scripts/AddUsersToMailboxes_FullAccessRights.ps1
@@ -0,0 +1,30 @@
+﻿#setting up default output and input paths
+
+$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
+$outputpath="$dir\output\"
+$inputpath="$dir\input\"
+
+###################
+#IMPORTANT:
+#locations of the imports will be in the location of the script files
+# SCRIPTFILELOCATION:\input\mailboxes
+#--mailboxes are referenced by alias
+# SCRIPTFILELCOATION:\input\users.csv
+#--users are referenced by user name/display name
+###################
+$groups = import-csv "$inputpath\mailboxes.csv"
+$users = import-csv "$inputpath\users.csv"
+
+#for each user add the user to the group with full access rights
+foreach($group in $groups){
+
+     $A = $group.Alias
+
+     foreach($user in $users){
+        $u = $user.User
+        Add-MailboxPermission -Identity "$A" -User "$u" -AccessRights FullAccess
+
+     }
+    
+}
